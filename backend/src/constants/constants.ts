7f171dc2635de5c06ee18936f216732b25ae67b8
ayush7801 <ayushtiwari1153@gmail.com>
2024-05-18 17:15:02 +0530

    updated frontend domain

--- backend/src/constants/constants.ts
+++ backend/src/constants/constants.ts
@@ -1,6 +1,8 @@
 export class Constants {
     public static readonly AUTH_COOKIE_NAME: string = 'auth-cookie';
 
+    public static readonly DOMAIN_NAME: string = 'dinogpt.netlify.app';
+
     public static readonly OPENAI_API_MODEL: string = 'gpt-3.5-turbo';
   
     private static instance: Constants;
