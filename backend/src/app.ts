19e7126c8cdd313e29db771990512b009f244b70
ayush7801 <ayushtiwari1153@gmail.com>
2024-05-19 18:02:59 +0530

    added updated domains in cors oring

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,8 +10,19 @@ config();
 
 const app = express();
 
+// Define your list of allowed origins
+const allowedOrigins = ['https://mern-openai-chatbot.onrender.com', 'https://dinogpt.site', 'https://www.dinogpt.site', 'http://localhost:3000' ];
+
 // Middlewares
-app.use(cors({origin: 'https://mern-openai-chatbot.onrender.com', credentials: true}));
+app.use(cors({origin: function(origin, callback){
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }, credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan('dev'));
